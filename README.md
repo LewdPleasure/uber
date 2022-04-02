09431e8713534c41ba3f9a57fee545c9152e2785
LewdPleasure <102863309+LewdPleasure@users.noreply.github.com>
2022-04-03 02:00:28 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+UBER777.MOSCOW
+
+Attempt to make layout
---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function() {
+
+  $('.burger').click(function(event) {
+    $('.burger, .burger__line, .nav__list').toggleClass('active');
+    // $('body').toggleClass('fixed');
+  });
+
+
+});
